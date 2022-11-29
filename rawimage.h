100dbc6c17eec9234e1d86d29f668a11b0bade49
EricsZhou <94219246+EricsZhou@users.noreply.github.com>
2022-11-29 11:53:45 +0800

    Create rawimage.h

---
+++ rawimage.h
@@ -0,0 +1,12 @@
+#ifndef _RAW_IMAGE_
+#define _RAW_IMAGE_
+
+// 获取Raw图片的均值
+template<typename T>
+double CalculateMean(T* raw, int width, int left, int top, int right, int bottom);
+
+// 获取Raw图片的最大值和最小值
+template<typename T>
+void GetMaxAndMin(T* raw, int width, int height, T& min, T& max);
+
+#endif // _RAW_IMAGE_
